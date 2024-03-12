4b9ab966ba4257d4ff0d7cef965257799264e26d
Vinicius Saes <viniciusaesouza@gmail.com>
2024-03-12 08:39:28 -0300

    Create decisoes.py

---
+++ aula_12_03/decisoes.py
@@ -0,0 +1,21 @@
+# import os
+# os.system('cls')
+
+# # NARRATIVA:
+# # -Pedir número
+# num = int(input("Numero: "))
+# if num < 0:
+#     # -Se o numero for negativo
+#     num = num * -1
+# # -Exibir numero
+# print(num)
+
+# valor_da_venda = int(input("Digite o valor do produto: "))
+
+# if valor_da_venda > 500:
+#     valor_final = 0
+#     valor_final = valor_da_venda - 50
+# print(f"""
+#     Valor da venda: R${valor_da_venda},00 
+#     Valor final: R${valor_final},00""")
+
