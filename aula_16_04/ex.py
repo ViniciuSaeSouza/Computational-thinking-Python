a7dca245476c2dfe81c1f9f666f8fdf81bea3134
Vinicius Saes <viniciusaesouza@gmail.com>
2024-04-16 12:37:33 -0300

    Create ex.py

---
+++ aula_16_04/ex.py
@@ -0,0 +1,33 @@
+'''
+IMPLEMENTAÇÕES
+- Colocar as porcentagens dos status
+- verificar se são numeros as notas
+- Exibir aprovado, reprovado e maximo do relatorio final em ordem decrescente
+- exibir os status do relatório final no singular ou plural
+'''
+
+"""
+Uma turma tem 10 alunos.
+Um professor desenvolveu uma rotina para ter um melhor gerenciamento o gerenciamento dos alunos.
+A média anual se dá por:
+- Checkpoint:
+    - São 3 avaliações
+    - Calcula-se a média comum das duas maiores notas
+    - Vale 20% da média final
+- Challenge:
+    - São 2 sprints
+    - O cálculo é a média simples das duas notas
+    - Vale 20% da média final
+- Global solution:
+    - Apenas uma nota que
+    - Vale 60% da média final.
+Requisitos:
+- A média final para passar é ao menos 6, senão estará reprovado
+- Toda vez que for digitada uma nota inválida (fora de 0 e 10), advertir
+  e pedir novamente a digitação da mesma nota.
+Relatório:
+- para cada aluno exibir as médias calculadas e se ele está aprovado ou não.
+- no final da digitação das notas dos dez alunos, exibir:
+    - quantos foram aprovados
+    - quantos foram reprovados
+    - quantos tiraram a media final maxima (nota 10)"""
