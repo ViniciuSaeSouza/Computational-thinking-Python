8126d961c0baceed4bd2155a76096ef40e0562d3
Vinicius Saes <viniciusaesouza@gmail.com>
2024-04-18 17:59:30 -0300

    update

--- aula_18_04/nivel2.py
+++ aula_18_04/nivel2.py
@@ -6,12 +6,16 @@ v2 = [0, 0, 0, 0, 0]
 
 
 #1 - 
-for i in range(0, 5, 1):
-    v2[i] = v1[i]
+# for i in range(0, 5, 1):
+#     v2[i] = v1[i]
 
-print(v2)
+# print(v2)
 
 
 # 2- 
+aux = 0 # Define o inicio do vetor1
 for i in range(5, 0, -1):
-    
+    v2[aux] = v1[i-1]
+    aux += 1
+
+print(v1, v2)    
