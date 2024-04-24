5abf6515b836f0438553998dd2da10b2844d8814
Vinicius Saes <viniciusaesouza@gmail.com>
2024-04-24 18:58:17 -0300

    Update

--- aula_18_04/ex.py
+++ aula_18_04/ex.py
@@ -3,20 +3,20 @@ v = [45, -89, 32, -12, 33]
 import os
 os.system('cls')
 
-# print(v)
+print(v)
 
-# for i in range(0, 5, 1):
-#     numero = input("Digite os números: ")
-#     numero = int(numero)
-#     v[i] = numero
-#     # print(f"Elemento[{i}]= {v[i]:6.2f}" )
-# print(v)
+for i in range(0, 5, 1):
+    numero = input("Digite os números: ")
+    numero = int(numero)
+    v[i] = numero
+    print(f"Elemento[{i}]= {v[i]:6.2f}" )
+print(v)
 
-# print(v[0])
+print(v[0])
 
-# for i in range (0, 5, 1):
-#     if v[i] < 0:
-#         print(v[i], end=" ")
+for i in range (0, 5, 1):
+    if v[i] < 0:
+        print(v[i], end=" ")
 
 # 3 / 4 -
 soma = 0
@@ -25,8 +25,8 @@ for i in range(0,5,1):
 media = soma / 5 
 print("Soma: ", soma)
 print("Media: ", media)
-# for i in range(0, 5, 1):
-#     media = soma / 5
+for i in range(0, 5, 1):
+    media = soma / 5
 
 # 5- 
 for i in range(0,5,1):
