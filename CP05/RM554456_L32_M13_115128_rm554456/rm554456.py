131a418ff0a941b1bf19731886290e74562354e0
Vinicius Saes <viniciusaesouza@gmail.com>
2024-10-03 12:18:48 -0300

    cp05

---
+++ CP05/RM554456_L32_M13_115128_rm554456/rm554456.py
@@ -0,0 +1,94 @@
+# RM 554456 - VINÍCIUS SAES DE SOUZA
+
+import os
+
+# SUB-ALGORITMOS
+
+def valida_credencial(dado: str, tipo: str) -> bool:
+    letras = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
+    letras = letras.split(",")
+    dado = dado.lower()
+    if tipo == 'l':
+        if len(dado) != 6:
+            return False
+        elif not dado[0].isnumeric():
+            return False
+        for char in dado[1:]:
+            if char in letras:
+                return True
+        else:
+            return True
+
+    if tipo == 's':
+        if len(dado) < 6:
+            return False
+        elif dado[0] not in letras:
+            return False
+        for char in dado[1:]:
+            if char.isnumeric():
+                return True
+        else:
+            return True
+
+def cadastrar_credencial() -> None:
+    os.system('cls')
+    while True:
+        login = input("Login: ")
+        if valida_credencial(login, 'l'):
+            break
+        else:
+            print("ERRO! Digite um login válido")
+    while True:
+        senha = input("Senha: ")
+        if valida_credencial(senha, 's'):
+            print("\nLogin e Senha cadastrado com sucesso!")
+            cria_arquivo(login, senha)
+            input("Digite qualquer tecla para voltar: ")
+            menu()
+            break
+        else:
+            print("ERRO! Digite uma senha válida")
+
+def cria_arquivo(login: str, senha: str) -> None:
+    with open("rm554456.txt", "a") as file:
+        pessoa = f"{login.upper()}, {senha}\n"
+        file.write(pessoa)
+
+def mostrar_arquivo():
+    with open("rm554456.txt", "r", encoding="utf-8") as file:
+        os.system('cls')
+        print(file.read())
+        input("Digite qualquer tecla para voltar ao menu: ")
+        menu()
+
+
+# MAIN
+def menu():
+    os.system('cls')
+    print("""MENU
+0 - SAIR
+1 - Digite as credenciais (Login e Senha)
+2 - Exibir o arquivo""")
+
+    escolha = input("Escolha: ")
+
+    while (True):
+        match escolha:
+            case "0":
+                print("Fechando programa...")
+                exit()
+            case "1":
+                cadastrar_credencial()
+            case "2":
+                mostrar_arquivo()
+            case _:
+                print("Escolha uma opção válida!")
+                menu()
+
+menu()
+
+"""
+34MARI, maria23!
+8483ES, mac3435
+5EDSON, abc12345
+"""
---
+++ CP06/Ativ1/atividade_01/atividade01.py
@@ -0,0 +1,108 @@
+# ax² + bx + c
+# a = 1 / b = -5 / c = 6
+import os
+import math
+from typing import Tuple
+
+def verifica_igual_zero(numero: float) -> bool:
+    if numero == 0:
+        return True
+    else:
+        return False
+
+def calcula_delta(a: float, b: float, c: float) -> float:
+    delta = (math.pow(b, 2)) - 4 * a * c
+    return delta
+
+
+def bhaskara(a: float, b: float, delta: float) -> tuple[float, float]:
+    x1 = (-b + math.sqrt(delta)) / (2 * a)
+    x2 = (-b - math.sqrt(delta)) / (2 * a)
+
+    return x1, x2
+
+def converte_float(item:str) -> float:
+
+    try:
+        numero = float(item)
+        return numero
+    except:
+        print("Favor digitar um número!")
+        input("Aperte qualquer tecla para recomeçar.")
+        menu()
+
+def verifica_equacao_incompleta(num1: float, num2: float) -> bool:
+    if verifica_igual_zero(num1) or verifica_igual_zero(num2):
+        return True
+    else:
+        return False
+
+def verifica_negativo(item:float) -> bool:
+    if item < 0:
+        return True
+    else:
+        return False
+
+def verifica_continuar(inpt: str) -> bool:
+    opcoes = ["S", "N"]
+    if inpt.upper() in opcoes:
+        return True
+    else:
+        return False
+
+
+def continuar_sistema():
+    continuar = input("Continuar executando o programa? [S]im ou [N]ão: ")
+    if not verifica_continuar(continuar):
+        if continuar.upper() == "S":
+            menu()
+        if continuar.upper() == "N":
+            print("Finalizando....")
+            exit()
+        else:
+            print("Digite uma opção válida!")
+            continuar_sistema()
+
+
+def menu():
+    os.system('cls')
+    while(True):
+        print(" -- EQUAÇÕES SEGUNDO GRAU -- ")
+        print("Digite as varáveis para calcular:")
+        a = converte_float(input("A: "))
+
+        if verifica_igual_zero(a):
+            print("Esta equação não é do segundo grau, sim do primeiro.")
+            break
+
+        b = converte_float(input("B: "))
+        c = converte_float(input("C: "))
+
+
+        delta = calcula_delta(a, b, c)
+        x1, x2 = bhaskara(a, b, delta)
+
+        if verifica_negativo(delta):
+            print(f"Delta 🔺: {delta}")
+            print("Não é possível calcular x1 e x2 porque o delta é negativo")
+
+
+        elif verifica_igual_zero(delta):
+            print(f"Delta 🔺: {delta}")
+            print(f"X1: {x1} \nX2: {x2}")
+            print("As raízes x1 e x2 tem o mesmo valor")
+
+        elif verifica_equacao_incompleta(b, c):
+            print(f"Delta 🔺: {delta}")
+            print(f"X1: {x1} \nX2: {x2}")
+            print("Equação do segundo grau INCOMPLETA")
+
+        else:
+            print(f"Delta 🔺: {delta}")
+            print(f"X1: {x1} \nX2: {x2}")
+            print("Equação do segundo grau COMPLETA\n As raízes x1 e x2 tem valores distintos")
+
+        continuar_sistema()
+
+
+menu()
